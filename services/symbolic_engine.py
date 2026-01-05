1ca65e0e72e60bc9a72436586153e9ff6751681e
rpahall86-cmd <rpahall86@gmail.com>
2026-01-05 15:53:48 +0000

    Removing replicated files

--- services/symbolic_engine.py
+++
@@ -1,8 +0,0 @@
-# services/symbolic_engine.py
-class SymbolicEngine:
-    def __init__(self, expression, symbols):
-        self.expression = expression
-        self.symbols = symbols
-
-    def evaluate(self, values: dict):
-        return self.expression.subs(values).evalf()
